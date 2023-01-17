43dd7d2384ec0e069cd54e6cc231bd0758992b5e
hp <hp@pop-os.localdomain>
2023-01-17 18:36:24 -0300

    feat: requisito 5- funcao get tech news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 # Requisito 1
 
@@ -69,4 +70,14 @@ def scrape_news(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    URL = "https://blog.betrybe.com/"
+    news = []
+    while len(news) <= amount:
+        site = fetch(URL)
+        news.extend(scrape_updates(site))
+        URL = scrape_next_page_link(site)
+    tech_news = []
+    for new in news[:amount]:
+        tech_news.append(scrape_news(fetch(new)))
+    create_news(tech_news)
+    return tech_news
